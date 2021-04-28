b178cc10a5eff0358c5c94878ea6ea5d0ac10f6d
sidhaler <62057963+sidhaler@users.noreply.github.com>
2021-04-28 22:28:34 +0200

    jms

---
+++ RUSTlib/src/main.rs
@@ -0,0 +1,46 @@
+use std::thread;
+use libc;
+use std::fmt;
+use std::io::{ErrorKind, Result};
+use std::net::{SocketAddr, TcpStream, IpAddr, Ipv4Addr};
+use std::time::Duration;
+use std::thread::Thread;
+
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum PortStats {
+    Open,
+    Closed,
+    Filtered,
+    BadHost,
+}
+
+
+#[tokio::main]
+fn main() -> std::io::Result<()>  {
+
+
+let net: String = "192.168.1.1".to_string();
+for e in 1..1000{
+    let mut addr = String::new();
+
+
+    addr = format!("{}:{}", net, e);
+
+    ///   let mut add = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192,168,1,1)), e);
+
+        thread::spawn(move ||{
+        ///    println!("Thread: nr. => {}", e);
+            if let Ok(stream) = TcpStream::connect(add) {
+                println!("PORT {} OPEN", e);
+            }
+
+
+           /// let dur = Duration::from_millis(100);
+
+            thread::sleep(Duration::from_millis(100));
+        });
+}
+    Ok(())
+}
+
