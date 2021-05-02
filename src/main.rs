aea30b4d44dd8d2aa99eb414a77d41b4698d411a
sidhaler <62057963+sidhaler@users.noreply.github.com>
2021-05-02 16:13:38 +0200

    better performance
    1000 ports ~ 2 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{thread, env};
-use std::io::{ErrorKind};
-use std::net::{SocketAddr, TcpStream, IpAddr, Ipv4Addr};
-use std::time::Duration;
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+use std::time::{Duration, Instant};
 use many::tcp::{PortStat, tcp_scan};
 use std::result::Result::Ok;
 
@@ -13,18 +12,21 @@ async fn main() -> std::io::Result<()>  {
     // target
     let wrr = &args[1];
     // range of ports
-    let portrange = &args[2];
+    let portrange= &args[2];
 
     //converting target to string from &string for later use
     let actddr = wrr.to_string();
 
-    //
-    println!("\nWelcome in JmSscanner, source => https://github.com/sidhaler/jmsSCANNER \n\n\r\r");
-    println!("SCANNING => {} || On TCP protocol || With range of {} ports \n\n\r\r", actddr, portrange);
-    println!("Resolving meaning of results↓\n\r*STATE* || *PORT NUMBER*/PROTOCOl \n\n\n\r\r\r");
-
+    println!(
+        "\r\nWelcome in JmSscanner, source => https://github.com/sidhaler/jmsSCANNER \n\n\r\r");
+    println!(
+        "SCANNING => {} || On TCP protocol || With range of {} ports \n\n\r\r"
+             , actddr, &portrange);
+    println!(
+        "Resolving meaning of results↓\n\r*STATE* || *PORT NUMBER*/PROTOCOl \n\n\n\r\r\r");
     // timeout duration
-    let timeout_dur = Duration::from_millis(5);
+    let timeout_dur = Duration::from_millis(2);
+
     // idk
     let mut c: i32 =  0;
 
@@ -32,14 +34,13 @@ async fn main() -> std::io::Result<()>  {
     let target: Ipv4Addr = actddr.parse().expect("Argument Error");
     let maxport: u16 = portrange.parse().expect("Argument Error");
 
-    println!("RESULTS OF SCAN ↓\n");
+    // getting date
+    let mut start = Instant::now();
+
+    println!("RESULTS OF SCANN ↓\n");
     for i in 1..maxport+1{
-
-
-
         // actual addr
         let add: SocketAddr = SocketAddr::new(IpAddr::V4(target), i);
-
         let t = thread::spawn(move ||{
             loop {
                 // MATCHING RESULTS FROM STREAM
@@ -56,11 +57,13 @@ async fn main() -> std::io::Result<()>  {
 
                 return
             }
-            thread::sleep(Duration::from_millis(5));
+            thread::sleep(Duration::from_millis(2));
+
         }
 
         );
         t.join().unwrap();
     };
+    println!("JmSscan done: in {} seconds scanned {} ports", start.elapsed().as_secs(), &maxport);
     Ok(())
 }
